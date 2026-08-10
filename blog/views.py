101d96eb91e190c8307675598f88d66ecedd89f0
ParsaRahimpour <parsarhmpr2019@gmail.com>
2026-08-10 15:04:49 +0330

    blog pages improved
    in blog home and blog single, images, date, and content format changed and also some new template tags used to improve the code.

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,27 @@ def blog_view(request):
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
+# def blog_single(request, pid):
+#     posts = Post.objects.filter(status=1)
+#     post = get_object_or_404(posts, pk=pid)
+#     context = {'post':post}
+#     return render(request, 'blog/blog-single.html', context)
+
+
 def blog_single(request, pid):
     posts = Post.objects.filter(status=1)
     post = get_object_or_404(posts, pk=pid)
-    context = {'post':post}
+    posts = list(posts)
+    current_index = posts.index(post)
+    previous_post = posts[current_index - 1] if current_index > 0 else None
+    next_post = posts[current_index + 1] if current_index < len(posts) - 1 else None
+    context = {
+        'post': post,
+        'previous_post': previous_post,
+        'next_post': next_post,
+    }
     return render(request, 'blog/blog-single.html', context)
+
 
 def test(request, pid):
     # post = Post.objects.all()
