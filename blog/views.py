ac1b312e4f5e6cb1b2ba6b77c57914fdd295cab0
ParsaRahimpour <parsarhmpr2019@gmail.com>
2026-08-15 16:50:32 +0330

    writing customized tags
    writing some customized tags and using them in popular posts

--- blog/views.py
+++ blog/views.py
@@ -28,8 +28,5 @@ def blog_single(request, pid):
     return render(request, 'blog/blog-single.html', context)
 
 
-def test(request, pid):
-    # post = Post.objects.all()
-    post = get_object_or_404(Post, pk=pid)
-    context = {'post':post}
-    return render(request, 'test.html', context)
+def test(request):
+    return render(request, 'test.html')
