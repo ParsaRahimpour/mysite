4b9ad751d0f4ee90974205337f2f94f11848a136
ParsaRahimpour <parsarhmpr2019@gmail.com>
2026-08-18 14:37:07 +0330

    adding search feature for content of blogs

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,37 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.utils import timezone
+from django.db.models import F
 from blog.models import Post
 
-def blog_view(request):
-    posts = Post.objects.filter(status=1)
+
+def blog_view(request, **kwargs):
+    posts = Post.objects.filter(
+        status=1,
+        published_date__lte=timezone.now())
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if  kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])    
+    posts = Paginator(posts, 3)  
+    try:
+        page_number = request.GET.get('page')  
+        posts = posts.page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
-# def blog_single(request, pid):
-#     posts = Post.objects.filter(status=1)
-#     post = get_object_or_404(posts, pk=pid)
-#     context = {'post':post}
-#     return render(request, 'blog/blog-single.html', context)
-
 
 def blog_single(request, pid):
-    posts = Post.objects.filter(status=1)
+    posts = Post.objects.filter(
+        status=1,
+        published_date__lte=timezone.now())
     post = get_object_or_404(posts, pk=pid)
+    Post.objects.filter(pk=post.pk).update(counted_view=F('counted_view') + 1)
+    post.refresh_from_db
     posts = list(posts)
     current_index = posts.index(post)
     previous_post = posts[current_index - 1] if current_index > 0 else None
@@ -26,6 +42,26 @@ def blog_single(request, pid):
         'next_post': next_post,
     }
     return render(request, 'blog/blog-single.html', context)
+
+
+def blog_category(request, cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts':posts}
+    return render(request, 'blog/blog-home.html', context)
+
+
+def blog_search(request):
+    # print(request.__dict__)
+    posts = Post.objects.filter(
+        status=1,
+        published_date__lte=timezone.now())
+    if request.method == 'GET':
+        if s:= request.GET.get('s'):
+            posts = posts.filter(content__contains=s)      
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
+
 
 
 def test(request):
