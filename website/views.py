b13117f196c1e0f28a0a12062657ffa20934426c
ParsaRahimpour <parsarhmpr2019@gmail.com>
2026-09-11 21:07:59 +0330

    Set message for contact form

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import *
 from website.models import Contact
 from website.forms import ContactForm, NewsletterForm
+from django.contrib import messages
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -14,6 +15,10 @@ def contact_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(request, messages.SUCCESS, 'Your ticket submited successfully')
+        else:
+            messages.add_message(request, messages.ERROR, 'Your ticket did not submited')
+
 
     form = ContactForm()        
     return render(request, 'website/contact.html', {'form':form})
