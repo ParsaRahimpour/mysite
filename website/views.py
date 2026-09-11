75d119f978aff0210ad444c70733f3edeeb1cf72
ParsaRahimpour <parsarhmpr2019@gmail.com>
2026-09-11 20:50:06 +0330

    fixing the form of newsletter
    add newsletter part to admin and fixing the form of newsletter with ModelForm

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import *
 from website.models import Contact
-from website.forms import ContactForm
+from website.forms import ContactForm, NewsletterForm
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -18,6 +18,17 @@ def contact_view(request):
     form = ContactForm()        
     return render(request, 'website/contact.html', {'form':form})
 
+
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
+
+           
 def test_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
